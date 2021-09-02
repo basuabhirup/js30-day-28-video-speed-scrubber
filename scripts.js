e70ed84fdbd02989b37ba841c31d1c7213a90504
basuabhirup <basu.abhirup@yahoo.com>
2021-09-02 15:10:41 +0530

    Set to change video playback rate.
    Update video playback rate against mousedown as well as mousemove on the speed bar.
    Set mouseup and mouseleave events to trigger the speed bar to stop changing the playback  rate.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 const speed = document.querySelector('.speed');
 const bar = document.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
+let isDown = false;
 
-speed.addEventListener('mousemove', function(e) {
+function changeRate(e) {
 // Normal functions implement 'this' property by themselves, unlike arrow functions.
-  const percentage = ((e.pageY - this.offsetTop) / this.offsetHeight) * 100;
-  const min = 0.4;
-  const max = 4;
-  const playRate = (min + (max - min)*percentage/100).toFixed(1);
-  bar.style.height = `${percentage}%`;
-  bar.textContent = `${playRate}x`;
-})
+  if (isDown) {
+    const percentage = ((e.pageY - this.offsetTop) / this.offsetHeight) * 100;
+    const min = 0.4;
+    const max = 4;
+    const playbackRate = (min + (max - min)*percentage/100).toFixed(1);
+    bar.style.height = `${percentage}%`;
+    bar.textContent = `${playbackRate}x`;
+    video.playbackRate = playbackRate;
+  }
+}
+
+speed.addEventListener('mousedown', () => isDown = true);
+
+speed.addEventListener('mousedown', changeRate);
+speed.addEventListener('mousemove', changeRate);
+
+speed.addEventListener('mouseleave', () => isDown = false);
+speed.addEventListener('mouseup', () => isDown = false);
